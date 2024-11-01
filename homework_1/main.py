5303ae38026cbd1f3d8513ade0c00fce8b589075
golueb <golubevegor80@gmail.com>
2024-11-01 15:33:06 +0300

    changed directory

--- homework_1/main.py
+++ homework_1/main.py
@@ -1,3 +1,15 @@
 class Animal:
     name = ''
     sound = ''
+    def make_sound(self):
+        print(self.sound)
+
+cat = Animal()
+cat.color = 'white'
+cat.sound = 'meow'
+dog = Animal()
+dog.sound = 'woof'
+dog.color = 'blue'
+
+cat.make_sound()
+dog.make_sound()
