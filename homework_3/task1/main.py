ffebcf0e0fbb1e97a13b80c00c71292aad6e94cd
golueb <golubevegor80@gmail.com>
2024-11-17 14:27:53 +0300

    task1 homework3 done

---
+++ homework_3/task1/main.py
@@ -0,0 +1,6 @@
+import  package.module as m
+
+List = [4,6,2,3,4,6,3,1,1]
+
+print(m.Summ_list.summ(List))
+
---
+++ homework_3/task1/package/module.py
@@ -0,0 +1,7 @@
+class Summ_list:
+    @staticmethod
+    def summ(user_list):
+        s=0
+        for i in user_list:
+           s+=i
+        return s
